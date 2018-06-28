84a973f3a3498ddf579bf80583ebc6c5f80696bb
Ahmed Dawuda <kofidahmed@gmail.com>
2018-06-28 19:33:26 +0000

    ready

--- sw.js
+++ sw.js
@@ -1,13 +1,20 @@
 'use strict';
 
+//cache version name
 var currentCache = 'currency-converter-v2';
 
+/**
+ * on installation, cache the asset files
+ */
 self.addEventListener('install', function (installation) {
     installation.waitUntil(caches.open(currentCache).then(function (cache) {
         return cache.addAll(['/', '/dist/bundle.js', '/dist/style.css', '/dist/logo.png']);
     }));
 });
 
+/**
+ * on activate, delete old caches
+ */
 self.addEventListener('activate', function (event) {
     event.waitUntil(caches.keys().then(function (cacheNames) {
         cacheNames.forEach(function (name) {
@@ -18,6 +25,9 @@ self.addEventListener('activate', function (event) {
     }));
 });
 
+/**
+ * on fetch response with a cached data or fetch data
+ */
 self.addEventListener('fetch', function (event) {
     event.respondWith(caches.match(event.request).then(function (response) {
         return response || fetch(event.request);
