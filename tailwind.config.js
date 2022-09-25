413de96d469bf62052f17a4ae7a097a596f86a1d
Kofi Adei <kofidahmed@gmail.com>
2022-09-25 06:19:15 -0400

    added home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./assets/**/*.{html,js}"],
+  content: ["index.html"],
   theme: {
     extend: {},
+    colors: ({ colors }) => ({
+      ...colors,
+      primary: '#28706C',
+      secondary: '#F26440',
+      default: '#F2F4F5'
+    }),
   },
   plugins: [],
 }
